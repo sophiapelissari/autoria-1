f904e506d0b5dcceecaa5c3e4116c313e30816d3
Sophia <sophiapelissari04@gmail.com>
2025-09-17 08:51:42 -0300

    feat: questão 3

---
+++ questão 3/questão 3/Program.cs
@@ -0,0 +1,11 @@
+﻿double dist;
+Console.WriteLine("Digite a distância percorrida em quilômetros:");
+dist = double.Parse(Console.ReadLine());// Verifica se a distância é diferente de 60
+if (dist != 60)
+{
+    Console.WriteLine("Distância fora do padrão esperado. :(");
+}
+else
+{
+    Console.WriteLine("Distância dentro do padrão esperado. :)");
+}
